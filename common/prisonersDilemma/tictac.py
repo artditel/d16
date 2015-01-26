570627f3ed49544706cd6fbf053bdae4cd8de6ef
lbezel <bezellisa@yandex.ru>
2015-01-26 09:57:01 +0300

    Update tictac.py

--- common/prisonersDilemma/tictac.py
+++ common/prisonersDilemma/tictac.py
@@ -1,5 +1,5 @@
 def tictac(my_moves, enemy_moves):
-		if False not in enemy_moves:
-			return False
-		else:
-            return random.random() > 0.8
+	if False not in enemy_moves:
+		return False
+	else:
+		return random.random() > 0.8
