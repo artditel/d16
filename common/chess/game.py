e784d385fd8e48ba02a0fe9c2c6e14a23c8000b5
Alex <savsmail@gmail.com>
2015-01-29 09:58:19 +0300

    chess added

---
+++ common/chess/game.py
@@ -0,0 +1,412 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+import random
+import time
+import sys
+import chess
+import baseline
+
+# ===================================================
+# scorers
+# ===================================================
+
+class ScorerWrapper:
+    WHITE_WINS_SCORE = 1000
+    BLACK_WINS_SCORE = -1000
+    DRAW_SCORE = 0
+
+    def __init__(self, scorer):
+        self.scorer = scorer
+        self.total_calls = 0
+        self.cache = {}
+
+    def calc(self, p):
+        zobrist_hash = p.zobrist_hash()
+        pos_hash = (zobrist_hash, p.transpositions[zobrist_hash])
+        if pos_hash in self.cache:
+            return self.cache[pos_hash]
+
+
+        self.total_calls += 1
+        if p.is_game_over():
+            if p.is_draw():
+                score = self.DRAW_SCORE
+            elif p.is_white_move():
+                score = self.BLACK_WINS_SCORE
+            else:
+                score = self.WHITE_WINS_SCORE
+        else:
+            score = self.scorer(p)
+
+        self.cache[pos_hash] = score
+        return score
+
+# ===================================================
+# core
+# ===================================================
+class Position(chess.Bitboard):
+    GREEN = '\033[92m'
+    RED   = '\033[91m'
+    ENDC  = '\033[0m'
+
+    def is_white_move(self):
+        return self.turn == chess.WHITE
+
+    def is_draw(self):
+        return self.is_stalemate()          or \
+            self.is_insufficient_material() or \
+            self.is_fivefold_repitition()   or \
+            self.is_seventyfive_moves()
+
+    def print_console(self):
+        move_num = self.fullmove_number
+        if self.is_white_move():
+            move_num -= 1
+        print( '%d.%s' % (move_num, ' ...' if self.is_white_move() else ''), self.peek() if len(self.move_stack) else "")
+        print()
+        s = self.fen().split()[0]
+        print( s \
+            .replace('/', '\n')   \
+            .replace('1', ' '*1*2)  \
+            .replace('2', ' '*2*2)  \
+            .replace('3', ' '*3*2)  \
+            .replace('4', ' '*4*2)  \
+            .replace('5', ' '*5*2)  \
+            .replace('6', ' '*6*2)  \
+            .replace('7', ' '*7*2)  \
+            .replace('8', ' '*8*2)  \
+            .replace('p', self.RED   + 'p' + self.ENDC + ' ') \
+            .replace('n', self.RED   + 'n' + self.ENDC + ' ') \
+            .replace('b', self.RED   + 'b' + self.ENDC + ' ') \
+            .replace('r', self.RED   + 'r' + self.ENDC + ' ') \
+            .replace('q', self.RED   + 'q' + self.ENDC + ' ') \
+            .replace('k', self.RED   + 'k' + self.ENDC + ' ') \
+            .replace('P', self.GREEN + 'p' + self.ENDC + ' ') \
+            .replace('N', self.GREEN + 'n' + self.ENDC + ' ') \
+            .replace('B', self.GREEN + 'b' + self.ENDC + ' ') \
+            .replace('R', self.GREEN + 'r' + self.ENDC + ' ') \
+            .replace('Q', self.GREEN + 'q' + self.ENDC + ' ') \
+            .replace('K', self.GREEN + 'k' + self.ENDC + ' '))
+        # print('\n==================================\n')
+
+
+# ===================================================
+# move listers
+# ===================================================
+class MoveLister(object):
+    def get(self, p, depth):
+        '''
+        @param  p     -- Position
+        @param  depth -- current depth
+        @return list of (move, move_score) tuples sorted accordingly
+        '''
+        raise NotImplemented('Must be implemented!')
+
+class TrivialLister(MoveLister):
+    def __init__(self, max_depth):
+        self.max_depth = max_depth
+
+    def get(self, p, depth):
+        if depth >= self.max_depth:
+            return []
+        return [(move, 0.) for move in p.legal_moves]
+
+class PositionScoreLister(MoveLister):
+    def __init__(self, scorer, depth_widths):
+        '''
+        @param score        -- descendant of PositionScorer
+        @param depth_widths -- list of total best moves left at each depth, 0 means all moves should be taken
+        '''
+        self.scorer       = scorer
+        self.depth_widths = depth_widths
+
+    def get(self, p, depth):
+        if depth >= len(self.depth_widths):
+            return []
+
+        scored_moves = []
+        for move in p.legal_moves:
+            p.push(move)
+            scored_moves.append((move, self.scorer.calc(p)))
+            p.pop()
+        random.shuffle(scored_moves)
+        scored_moves.sort(key=lambda t: t[1], reverse=p.is_white_move())
+
+        width = self.depth_widths[depth]
+        return scored_moves if width == 0 else scored_moves[:width]
+
+# ===================================================
+# move finders
+# ===================================================
+class MoveFinder(object):
+    def __init__(self, scorer):
+        self.scorer = scorer
+
+    def _find(self, p):
+        raise NotImplemented('Must be implemented!')
+
+    def find(self, p):
+        self.scorer.total_calls = 0
+        return self._find(p)
+
+class DepthWidthMoveFinder(MoveFinder):
+    def __init__(self, scorer, lister):
+        super(DepthWidthMoveFinder, self).__init__(scorer)
+        self.lister = lister
+
+    def _find(self, p, depth=0):
+        considered_moves = self.lister.get(p, depth)
+        if len(considered_moves) == 0:
+            return None, self.scorer.calc(p)
+
+        scored_variations = []
+        for move, move_score in considered_moves:
+            p.push(move)
+            scored_variations.append((move, self._find(p, depth + 1)[1]))
+            p.pop()
+
+        random.shuffle(scored_variations)
+        scored_variations.sort(key=lambda t: t[1], reverse=p.is_white_move())
+
+        #if depth == 0:
+        #    print(scored_variations[:3])
+
+        return (max if p.is_white_move() else min)(scored_variations, key=lambda t: t[1])
+
+class BruteForceFinder(MoveFinder):
+    def __init__(self, scorer, depth):
+        super(BruteForceFinder, self).__init__(scorer)
+        self.depth = depth
+
+    def _calc_best_move_score(self, position, recursion_depth = 2):
+        possible_moves = position.legal_moves
+        if recursion_depth == 0 or len(possible_moves) == 0:
+            return None, self.scorer.calc(position)
+
+        move_scores = []
+        for move in possible_moves:
+            position.push(move)
+            score = self._calc_best_move_score(position, recursion_depth - 1)[1]
+            move_scores.append((move, score))
+            position.pop()
+
+        random.shuffle(move_scores)
+        move_scores.sort(key=lambda move_score: move_score[1], reverse=position.is_white_move())
+
+        if recursion_depth == self.depth:
+            print(move_scores[:3])
+
+        return move_scores[0]
+
+    def _find(self, position):
+        move, score = self._calc_best_move_score(position, self.depth)
+        return move, score
+
+
+
+class AlphaBetaNode():
+    def __init__(self, scorer, position, is_list = False):
+        if is_list:
+            self.moves = []
+        else:
+            move_scores = []
+            for move in position.legal_moves:
+                position.push(move)
+                score = scorer(position)
+                move_scores.append((move, score))
+                position.pop()
+            move_scores.sort(key=lambda move_score: move_score[1], reverse=position.is_white_move())
+
+            self.moves = [move for move,s in move_scores]
+
+        self.min = ScorerWrapper.BLACK_WINS_SCORE
+        self.max = ScorerWrapper.WHITE_WINS_SCORE
+        self.white = position.is_white_move()
+        self.cur_ind = -1
+        self.started_process = False
+
+    def next(self):
+        self.cur_ind += 1
+        if self.cur_ind >= len(self.moves):
+            return None
+        return self.moves[self.cur_ind]
+
+class AlphaBetaFinder(MoveFinder):
+    def __init__(self, scorer, depth):
+        super(AlphaBetaFinder, self).__init__(scorer)
+        self.depth = depth
+
+    def _find(self, position):
+
+        best_move, expected_score = 0, 0
+        nodes_stack = [AlphaBetaNode(self.scorer, position)]
+
+        while len(nodes_stack) > 0:
+            upper = nodes_stack[-1]
+
+            if not upper.started_process:
+                upper.started_process = True
+                if len(upper.move_scores) > 0:
+                    next_move = nodes_stack[-1].next()
+                    position.push(next_move)
+                    nodes_stack.push(AlphaBetaFinder(self.scorer, position, len(nodes_stack) > self.depth))
+                else:
+                    #list processing
+                    upper.min = upper.max = self.scorer(position)
+
+                    min_i = len(nodes_stack)
+
+                    new_stack_length = len(nodes_stack)
+                    #update all nodes min-max
+                    for i in range(len(nodes_stack)-2 : -1 : -1):
+                        if nodes_stack[i].white:
+                            nodes_stack[i].min = max(nodes_stack[i].min, nodes_stack[i+1].min)
+                            if nodes_stack[i].max > :
+                                pass
+                        else:
+                            nodes_stack[i].max = min(nodes_stack[i].max, nodes_stack[i+1].max)
+
+
+
+
+
+            if next_move is None:
+                nodes_stack.pop()
+                position.pop()
+            else:
+
+
+
+
+
+
+
+
+
+
+
+
+        possible_moves = position.legal_moves
+        if recursion_depth == 0 or len(possible_moves) == 0:
+            return None, self.scorer.calc(position)
+
+        move_scores = []
+        for move in possible_moves:
+            position.push(move)
+            score = self._calc_best_move_score(position, recursion_depth - 1)[1]
+            move_scores.append((move, score))
+            position.pop()
+
+        random.shuffle(move_scores)
+        move_scores.sort(key=lambda move_score: move_score[1], reverse=position.is_white_move())
+
+        if recursion_depth == self.depth:
+            print(move_scores[:3])
+
+        return move_scores[0]
+
+
+
+class ManualFinder(MoveFinder):
+    def __init__(self, default_finder):
+        self.default_finder = default_finder
+        super(ManualFinder, self).__init__(lambda x: 0) #mock score
+
+    def _find(self, position):
+        position.print_console()
+        print("possible_moves:")
+
+        possible_moves = sorted(list(position.legal_moves), key=str)
+        move_by_str = {str(move):move for move in possible_moves}
+        def format_func(ind_move):
+            return "%d: %s" % ind_move
+        print(list(map(format_func, enumerate(possible_moves, start=1))))
+
+
+        success = False
+        while not success:
+            if sys.version.startswith('2'):
+                #python 2 compatibility
+                universal_input = raw_input
+            else:
+                universal_input = input
+
+            inp = universal_input("Print move or it's index. Or q to quit. Or just nothing (and enter) for auto-move: ").strip()
+            if inp.startswith('q'):
+                print("exiting")
+                sys.exit(0)
+            elif inp == "":
+                return self.default_finder._find(position)
+            elif inp in move_by_str.keys():
+                return move_by_str[inp], 0 #mock score
+            else:
+                try:
+                    num = int(inp)
+                    move = possible_moves[num - 1]
+                    success = True
+                except:
+                    print("Bad input. Move or index ({}-{}) or q expected".format(1, len(position.legal_moves)))
+        return move, 0 #mock score
+
+# ===================================
+# TGame
+# ===================================
+class Game(object):
+    def __init__(self, white_finder, black_finder, verbose=True, step_by_step=False, step_by_advantage=False):
+        self.position = Position()
+        self.white_finder = white_finder
+        self.black_finder = black_finder
+        self.verbose      = verbose
+        self.step_by_step = step_by_step
+        self.step_by_advantage = step_by_advantage
+
+    def run(self):
+        position_scores = []
+
+        while not self.position.is_game_over():
+            start  = time.time()
+            finder = self.white_finder if self.position.is_white_move() else self.black_finder
+            move, score = finder.find(self.position)
+            self.position.push(move)
+            if self.verbose:
+                self.position.print_console()
+                total_calls = finder.scorer.total_calls
+                total_time  = time.time() - start
+                position_scores.append(score)
+                print('calls :', total_calls)
+                print('time  :', total_time)
+                print('speed :', total_calls / total_time)
+                print('score :', position_scores[-1])
+                print('\n=========== *** ===========\n')
+
+            if self.step_by_step:
+                input("Press Enter to continue...")
+            if score != 0 and self.step_by_advantage:
+                input("Advantage! Press Enter to continue...")
+        return self.get_result()
+
+    def get_result(self):
+        if not self.position.is_game_over():
+            raise ValueError('Game not over yet!')
+        if self.position.is_draw():
+            return 0.5, 0.5
+        if self.position.is_white_move():
+            return 0, 1
+        return 1, 0
+
+def run_once(first, second):
+    w, b = Game(first, second, verbose=True, step_by_step=False, step_by_advantage=False).run()
+    print('Result', w, ':', b)
+
+if __name__ == '__main__':
+#    mode = 'compare'
+    mode = 'once'
+
+
+    first_scorer  = SCORERS_WRAPPED['material']
+    second_scorer = SCORERS_WRAPPED['material']
+    (run_once if mode == 'once' else run_compare)(
+#        ManualFinder(DepthWidthMoveFinder(first_scorer, PositionScoreLister(first_scorer, [0, 10]))),
+        DepthWidthMoveFinder(first_scorer, PositionScoreLister(first_scorer, [0, 10])),
+        BruteForceFinder(second_scorer, 2),
+    )
