b0947d19a5f70729bfeb808d8faf0105988a7e07
eshuvaeva <Elizshuvaeva@yandex.ru>
2015-02-05 10:10:33 +0300

    Update king_game.py

--- eshuvaeva/games/king_game.py
+++ eshuvaeva/games/king_game.py
@@ -5,19 +5,16 @@ winning_positions = []
 for i in range(width):
 	winning_positions.append([])
 	for t in range(length):
-		winning_positions[i].append(0)
-winning_positions[width-1][0] == 2
+		winning_positions[i].append(2)
 
 for i in range(width):
 	for g in range(length):
-		if (g>3 and winning_positions[i][g-3] == 2) or \
-		(g > 3 and winning_positions[i-1][g-3] == 2) or \
-		(g > 2 and winning_positions[i-2][g-2] == 2) or \
-		(g > 2 and winning_positions[i-1][g-1] == 2) or \
-		winning_positions[i-1][g] == 2:
+		if (g>2 and winning_positions[i][g-3] == 2) or \
+		(g > 2 and i>0 and winning_positions[i-1][g-3] == 2) or \
+		(g > 1 and i>1 and winning_positions[i-2][g-2] == 2) or \
+		(g > 0 and i>0 and winning_positions[i-1][g-1] == 2) or \
+		(i>0 and winning_positions[i-1][g] == 2):
 			winning_positions[i][g] = 1
-		else:
-			winning_positions[i][g] = 2
 
 for i in reversed(winning_positions):
-	print(i) 
+	print(i)
