332b72fc4b2142d6f8deff86d84aca39f947f7e4
Alex <savsmail@gmail.com>
2015-02-05 08:56:05 +0300

    chess updated

--- common/chess/baseline.py
+++ common/chess/baseline.py
@@ -1,4 +1,5 @@
 import random
+import chess
 
 # ===================================================
 # scorers
@@ -13,9 +14,12 @@ PIECE_2_SCORE = {
     'b': 3,
     'r': 5,
     'q': 9,
+    'k': 0,
 }
 
-def calc_by_bitboard(p):
+def material_scorer(p):
+    print(p.get_pieces())
+
     s = 0
     for square in chess.SQUARES:
         piece = p.piece_at(square)
@@ -24,19 +28,3 @@ def calc_by_bitboard(p):
         s += PIECE_2_SCORE[piece.symbol().lower()] * (1 if piece.color == chess.WHITE else -1)
     return s
 
-def calc_by_fen(p):
-    s = 0.
-    for c in p.fen().split()[0]:
-        if c.lower() not in 'pnbrq':
-            continue
-        is_white = c.isupper()
-        s += PIECE_2_SCORE[c.lower()] * (1 if is_white else -1)
-    return s
-
-def material_scorer(p):
-    use_fen = True
-
-    return calc_by_fen(p) if use_fen else calc_by_bitboard(p)
-
-def titanic(p):
-    return random.uniform(-1, 1)
