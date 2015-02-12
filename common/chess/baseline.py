36afd92ca8bf7c7e7dcd8b0ca952f508cdfd202e
tbaikabulov <timuret@yandex.ru>
2015-02-12 09:42:17 +0300

    Update baseline.py

--- common/chess/baseline.py
+++ common/chess/baseline.py
@@ -70,18 +70,22 @@ def titanic(p):
     return random.uniform(-1, 1)
 def Timur(p):
     s=0
-    bish=0
+    bish=[0,0]
     pawn=0    
     pieces = p.get_pieces()
     for square in chess.SQUARES:
             piece = p.piece_at(square)
             if piece == None:
                 continue
-            if (piece.symbol()=='B' and piece.color == chess.WHITE) or (piece.symbol()=='b' and piece.color == chess.BLACK):
-                bish+=1
+            if (piece.symbol()=='B' and piece.color == chess.WHITE):
+                bish[0]+=1
+            if (piece.symbol()=='b' and piece.color == chess.BLACK)
+               bish[1]+=1      
             s += PIECE_2_SCORE[piece.symbol().lower()] * (1 if piece.color == chess.WHITE else -1) 
-    if bish==2:
-        s+=(1/3)* (1 if piece.color == chess.WHITE else -1)
+    if bish[0]==2:
+        s+=(1/3)
+    if bish[1]==2:
+        s-=(1/3)
     return s
 
 def material_scorer(p):
