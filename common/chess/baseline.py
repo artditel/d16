a070c1400bd373865a4a26f1b5c631ae413cd3ab
azhuravlev1 <zhrav@me.com>
2015-02-19 10:45:05 +0300

    Update baseline.py

--- common/chess/baseline.py
+++ common/chess/baseline.py
@@ -175,11 +175,6 @@ class artemka_scorer_class(EasyScorer):
         return 1 + i/12
 
 class az(EasyScorer):
-    def get_pawn_score(self, i, j):
-        if len(self.p.move_stack) <30:
-            return 1-i/10
-        else:
-            return 1+i/10
     def get_king_score(self, i, j):
         if len(self.p.move_stack) <27:
             return -i/10
