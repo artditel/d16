c3f5fbe00bfed1f9586af9e0d47a7c7c81a1610d
Alex <savsmail@gmail.com>
2015-02-25 11:11:23 +0300

    auto csv generating added.

--- savfod/daemons/pdf_daemon.py
+++ savfod/daemons/pdf_daemon.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+import argparse
+import csv
 import os
 import shutil
 import subprocess
@@ -5,9 +8,63 @@ import sys
 import time
 import file_changes_watcher
 
-WATCH_DIRECTORIES = ['/home/alex/Dropbox/d16-calculus', '/home/alex/Dropbox/d16-programming']
+WATCH_DIRECTORIES = ['Dropbox/d16-calculus', 'Dropbox/d16-programming']
 STOPWORDS = ['related']
 WAIT_TIME = 10
+
+from csv_to_tex_daemon import PUPILS_NAMES
+
+class TaskInfo:
+	ITEMS_NAMES = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+	def __init__(self, num, difficulty=0):
+		self.num = num
+		self.difficulty = difficulty
+		self.items_difficulties = []
+	def add_item(self, difficulty=0):
+		self.items_difficulties.append(self.difficulty + difficulty)
+	def get_items_strings(self):
+		if len(self.items_difficulties) == 0:
+			return [str(self.num) + '*' * self.difficulty]
+		return ["{num}{item}{diff}".format(
+			                                   num=self.num,
+			                                   item=self.ITEMS_NAMES[i],
+			                                   diff='*' * diff
+		                                   ) for i, diff in enumerate(self.items_difficulties)]
+
+def get_task_message_parts(task_message):
+	parts = task_message.split('_')
+	num = int(parts[1])
+	difficulty = len(parts[2])
+	return num, difficulty
+
+def read_tasks(log_path):
+	tasks = {}
+	with open(log_path, encoding="cp1251") as log:
+		words = log.read().split()
+		for w in words:
+			if w.startswith('TaskFound'):
+				num, difficulty = get_task_message_parts(w)
+				tasks[num] = TaskInfo(num, difficulty)
+			elif w.startswith('TaskItemFound'):
+				num, difficulty = get_task_message_parts(w)
+				tasks[num].add_item(difficulty)
+	return tasks
+
+def save_tasks_to_csv(tasks, csv_path):
+	header = ["", ""]
+	for key in sorted(tasks.keys()):
+		header += tasks[key].get_items_strings()
+
+	with open(csv_path, 'w') as csv_out:
+		writer = csv.writer(csv_out, delimiter='\t')
+		writer.writerow(header)
+		for p in PUPILS_NAMES:
+			row = p.split() + [""]*(len(header) - 2)
+			writer.writerow(row)
+
+def gen_csv(log_path, csv_path):
+	tasks = read_tasks(log_path)
+	save_tasks_to_csv(tasks, csv_path)
 
 def make_pdf(file_path):
 	directory, file_name = os.path.split(file_path)
@@ -15,6 +72,7 @@ def make_pdf(file_path):
 	pdf_name = file_name[:-4] + '.pdf'
 	log_path = file_path[:-4] + '.log'
 	pdf_path = file_path[:-4] + '.pdf'
+	csv_path = file_path[:-4] + '.csv'
 
 	try:
 		os.remove(pdf_name)
@@ -35,19 +93,31 @@ def make_pdf(file_path):
 		except FileNotFoundError:
 			pass
 		shutil.move(pdf_name, pdf_path)
+		gen_csv(log_name, csv_path)
 	else:
 		print("error")
 		shutil.move(log_name, log_path)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Finds new tex-files and pdflatexs them')
+    parser.add_argument('-r', '--root', type=str, default="/home/sav/", help = "root dir for file searching")
+    parser.add_argument('-d', '--debug', action='store_true', help = "debug mode. Stops working if error occurs")
+    return parser.parse_args()
+
 def main():
-	watcher = file_changes_watcher.file_changes_watcher(WATCH_DIRECTORIES, ".tex", stopwords=STOPWORDS)
+	parser=parse_args()
+
+	watch_dirs = [os.path.join(parser.root, d) for d in WATCH_DIRECTORIES]
+	watcher = file_changes_watcher.file_changes_watcher(watch_dirs, ".tex", stopwords=STOPWORDS)
+
 	while True:
 		try:
 			for file_path in watcher.get_changed_files():
 				make_pdf(file_path)
 		except Exception as e:
 			print(sys.exc_info())
-			raise e
+			if parser.debug:
+				raise e
 
 		time.sleep(1)
 
