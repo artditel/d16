61e7fbc54d23e817c360a87c6b007fde10921272
lbezel <bezellisa@yandex.ru>
2015-02-26 10:23:04 +0300

    Update baseline.py

--- common/chess/baseline.py
+++ common/chess/baseline.py
@@ -70,23 +70,23 @@ class EasyScorer:
 
 class titanic(EasyScorer):
     def get_pawn_score(self, i, j):
-        if len(self.p.move_stack) >= 50:
+        if self.fullmove_number >= 50:
             return i**(1/2)
         else:
             return 1 + i**(1/4)
     def get_knight_score(self, i, j):
-        if len(self.p.move_stack) >= 50:
+        if self.fullmove_number >= 50:
             return 2
         else:
             return 3
     def get_bishop_score(self, i, j):
-        if len(self.p.move_stack) >= 50:
-            return 4 - (i - j)**(2/5)
+        if self.fullmove_number >= 50:
+            return 4 - (1/(i - j)**5)**2
         else:
-            return 3 - (i - j)**(2/5)
+            return 3 - (1/(i - j)**3)**2
     def get_rook_score(self, i, j):
-        if len(self.p.move_stack) >= 50:
-            return 5,5
+        if self.fullmove_number >= 50:
+            return 5.5
         else:
             return 5
 
