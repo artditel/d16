b91188bad735580a6172a9d69076cf300dfe8dee
Еремей <keremey@mail.ru>
2015-04-09 10:28:56 +0300

    Update fast_tasks_tests.py

--- common/fast_tasks_tests.py
+++ common/fast_tasks_tests.py
@@ -1,9 +1,15 @@
-#Write your functions here: they should be named parse_ints
-#parse_floats, filter_title_words, arithmetic_mean, arithmetic_median
+#Write your functions here: they should be named parse_ints,
+#parse_floats, filter_title_words, arithmetic_mean, arithmetic_median,
+#positive_ammount, positive_percent
 
 def hello_world():
     return "HelloWorld"
 
+def parse_ints(line):
+	return(list(map(int , line.split())))
+
+def parse_floats(line):
+	return(list(map(float , line.split())))
 
 assert(hello_world() == "HelloWorld")
 assert(tuple(parse_ints("1 2 3 4")) == (1, 2, 3, 4))
@@ -15,9 +21,17 @@ assert(tuple(parse_floats("38.0 15.96 3.005")) == (38.0, 15.96, 3.005))
 assert(tuple(filter_title_words("Abigale barse Carter durry Emerson flat Grey")) == ("Abigale", "Carter", "Emerson", "Grey"))
 assert(tuple(filter_title_words("Titanic Normandy steamer captain Great Eastern sea")) == ("Titanic", "Normandy", "Great", "Eastern"))
 assert(tuple(filter_title_words("accordeon Paris metropolitain Rouene arc du Triumph le Tour d Eiffel")) == ("Paris", "Rouene", "Triumph", "Tour", "Eiffel"))
-assert(tuple(arithmetic_mean([1.0, 3.0, 5.0])) == 3.0)
+assert(tuple(arithmetic_mean([1.0, -3.0, 5.0])) == 1.0)
 assert(tuple(arithmetic_mean([23.0, 5.0, 14.0])) == 14.0)
 assert(tuple(arithmetic_mean([1.6 , 4.0 , 6.8 , 5.7])) == 4.525)
-assert(tuple(arithmetic_median([1.0, 3.0, 5.0])) == 3.0)
+assert(tuple(arithmetic_median([1.0, 3.0, -5.0])) == 1.0)
 assert(tuple(arithmetic_median([23.0, 5.0, 14.0 , 4.0 , 6.8])) == 6.8)
-assert(tuple(arithmetic_median([1.6 , 4.0 , 6.8 , 5.7])) == 4.85)
+assert(tuple(arithmetic_median([-1.6 , 4.0 , 6.8 , 5.7])) == 4.85)
+assert(tuple(int(positive_ammount([-1.0, -3.0, 5.0]))) == 1)
+assert(tuple(int(positive_ammount([23.0, -5.0, 14.0, -14.0]))) == 2)
+assert(tuple(int(positive_ammount([1.6 , -4.0 , 6.8 , -5.7, -4.655, 6.894]))) == 3)
+assert(tuple(float(positive_percent([-1.0, 5.0]))) == 50.0)
+assert(tuple(float(positive_percent([23.0, -5.0, -14.0, -14.0]))) == 25.0)
+assert(tuple(float(positive_percent([1.6 , 4.0 -5.7, 4.655, 6.894]))) == 80.0)
+
+print("No errors detected, all tests finished correctly")
