6b9400b2cf476f51079271107d656c60e0036be8
Alex <savsmail@gmail.com>
2015-04-13 08:59:55 +0300

    Counter class tests added

--- common/fastTasks/classes.py
+++ common/fastTasks/classes.py
@@ -18,6 +18,19 @@ def test_hello_world():
     hw = HelloWorld("Vasya")
     assert(hw.get_hello() == "Hello world from Vasya")
 
+    print("HelloWorld class tested")
+
+def test_counter():
+    def hw():
+        print "Hello World!"
+    counter = Counter(hw)
+    counter()
+    assert(counter.get_calls_count() == 1)
+    counter()
+    assert(counter.get_calls_count() == 2)
+    print("Counter class tested")
+
+
 
 ###############################################################################
 #main. Add test calls here.
