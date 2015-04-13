0eea37045abc0f7f79886f71bfe94702c9c21efe
tbaikabulov <timuret@yandex.ru>
2015-04-13 09:35:11 +0300

    Update Vector2.py

--- tbaikabulov/Vector2.py
+++ tbaikabulov/Vector2.py
@@ -6,12 +6,14 @@ import par
 def distance (x1,y1,x2,y2):
     return ((x1-x2)**2+(y1-y2)**2)**0.5  
 WIDTH=800
-HEIGHT=900
+HEIGHT=700
 w,h=WIDTH,HEIGHT/2
 FPS = 100
-g=1
+g=0
 G=0
-ko=1/100
+ko=1/10
+KO=1/10
+activ=0
 HOLE_FILL='black'
 FIELD_COLOR='lime green'
 BORDER_COLOR='dark green'
@@ -20,12 +22,17 @@ def  S(a,b,c):
     return (p*(p-a)*(p-b)*(p-c))**0.5
 def distancel(x1,y1,x2,y2,x,y):
     return S( ((x-x1)**2+(y-y1)**2)**0.5,((x2-x1)**2+(y2-y1)**2)**0.5,((x-x2)**2+(y-y2)**2)**0.5)/((x2-x1)**2+(y2-y1)**2)**0.5
+def distance(x1,y1,x2,y2):
+    return ((x1-x2)**2 + (y1-y1)**2)**0.5
 class Vector:
     def __init__(self,x,y):
             self.x=x
             self.y=y
     def __add__(self,other):
-        return Vector(self.x+other.x,self.y+other.y)
+        if type(other)==Vector:
+            return Vector(self.x+other.x,self.y+other.y)
+        if type(other)==Ball:
+            return Ball(other.x+self.x,other.y+self.y,other.v,other.m,other.fill,other.R)
     def __mul__(self,k):
         return Vector(self.x*k,self.y*k)
     def proj(self,a):
@@ -34,6 +41,8 @@ class Vector:
         return Vector( (self.x*cos-self.y*sin)*cos,(self.x*cos-self.y*sin)*sin)
     def __abs__(self):
         return (self.x**2+self.y**2)**0.5
+    def __pow__(self,k):
+        return Vector(self.x**k,self.y**k)
 class Ball:
     force=Vector(0,0)
     def __init__(self,x,y,v,m,R,fill):
@@ -52,8 +61,14 @@ class Ball:
         return Vector(s*sin,-s*cos)    
     def distance(self,other):
         return ((self.x-other.x)**2+(self.y-other.y)**2)**0.5
+    def distance_next(self,other):
+        x1=self.x+self.v.x*ko+self.force.x*(0.5/self.m)*ko
+        x2=other.x+other.v.x*ko+other.force.y*(0.5/other.m)*ko
+        y1=self.y+self.v.y*ko+self.force.x*(0.5/self.m)*ko
+        y2=other.y+other.v.y*ko+other.force.y*(0.5/other.m)*ko        
+        return distance(x1,y1,x2,y2) 
     def E(self):
-        return (self.v.x**2+self.v.y**2)*self.m/2
+        return (self.v.x**2+self.v.y**2)*self.m/2-self.y*g*self.m
     def normal_distance(self,other):
         sin=(other.y-self.y)/self.distance(other)
         cos=(other.x-self.x)/self.distance(other)
@@ -72,7 +87,7 @@ class Rope:
             self.b1=b1
             self.b2=b2
     def force(self):
-            return (self.b1.distance(self.b2)- self.length)*bool(self.b1.distance(self.b2)- self.length>0)*0.1
+            return (self.b1.distance(self.b2)- self.length)*bool(self.b1.distance(self.b2)- self.length>0)*0.01
 class Spoke:
     def __init__(self,x1,y1,x2,y2,Balls):
                 self.x1=x1
@@ -89,7 +104,7 @@ def draw_field(canvas,game):
     for s in Spring:
         canvas.create_line(s.b1.x,s.b1.y,s.b2.x,s.b2.y,width=3,fill='dim gray')
     for r in Rope:
-        while r.b1.distance(r.b2)>=r.length:
+        if r.b1.distance(r.b2)>=r.length:
             canvas.create_line(r.b1.x,r.b1.y,r.b2.x,r.b2.y)
         else:
             if r.b2.x>r.b1.x:
@@ -102,41 +117,38 @@ def draw_field(canvas,game):
             canvas.create_line(j.x,j.y,j.x+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).x,j.y+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).y)
             canvas.create_oval(j.x+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).x+3,j.y+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).y+3,j.x+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).x-3,j.y+j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2).y-3)
             c.create_text(100,100,text=str(abs(j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2))))
-def change(v0,m0,v1,m1):
-    u0=(2*m1*v1+v0*(m0-m1))*(1/(m0+m1))
-    u1=(2*m0*v0+v1*(m1-m0))*(1/(m0+m1))
-    return(u0,u1)
+            c.create_text(100,200,text=str(sum([b.E() for b in game])))
 def make_move(game,c):
+    global activ
     global Game
-    global k
-    global G
-    print(1)
+    global ko
     for r in Rope:
-        if r.b1.distance(r.b2)>r.length and 1==0:
+        if r.b1.distance(r.b2)>r.length:
             r.b1.force += r.b1.vec_to(r.b2) * r.force()
             r.b2.force += r.b1.vec_to(r.b1) * r.force()    
     for i in game:
         i.force+=Vector(0,g)
+        i.force+=(i.v**2)*(-0.001)
         if not i.R<i.x+i.v.x*ko<WIDTH-i.R:
             pass
             i.v+=Vector(-2*i.v.x,0)
         if not i.R<i.y+i.v.y*ko<HEIGHT-i.R:
             pass
             i.v+=Vector(0,-2*i.v.y)
-    print(2)
     for s in Spring:
         s.b1.force += s.b1.vec_to(s.b2) * s.force()
         s.b2.force += s.b1.vec_to(s.b1) * s.force() 
     for sp in Spoke:
         for j in sp.Balls:
-            if 1==0:
-                j.force+=j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2)
+            j.force+=j.vec_to_line(sp.x1,sp.y1,sp.x2,sp.y2)
     for b1 in game:
         for b2 in game:
-            if b1!=b2 and 1==0:
+            if b1!=b2:
                 b1.force += b1.vec_to(b2) * G * b1.m * b2.m *(1/ (b1.distance(b2) ** 2))
-                b2.force += b2.vec_to(b2) * G * b2.m * b1.m *(1/ (b2.distance(b1) ** 2))                
-                if b1.distance(b2)<b1.R+b2.R:
+                b2.force += b2.vec_to(b2) * G * b2.m * b1.m *(1/ (b2.distance(b1) ** 2))
+                if (b1.v+b1).distance(b2.v+b2)<(b1.R+b2.R):
+                    activ=2
+                if b1.distance(b2)<(b1.R+b2.R):
                     b1.force += (b1.vec_to(b2)+b1.normal_distance(b2)*-1 )*(1/10)
                     b2.force += (b1.vec_to(b1)+b2.normal_distance(b1)*-1  )*(1/10)                  
     for b in game:
@@ -144,7 +156,12 @@ def make_move(game,c):
         b.force=Vector(0,0)
         b.x+=b.v.x*ko
         b.y+=b.v.y*ko
-    #print(sum([b.E() for b in game]))
+    if activ>0:
+        ko=1/500
+        activ-=1
+    else:
+        ko=KO
+    print(sum([b.E() for b in game]))
 if __name__ == "__main__":
     c = tkinter.Canvas(width=WIDTH, height=HEIGHT)
     c.pack()
@@ -157,17 +174,17 @@ def loop():
         make_move(Game,c)
     c.after(1000// FPS, loop)
 A=Ball(400,300,             Vector(-1,3),1,40,'orange')
-B=Ball(500,200,             Vector(1,0),1,30,'red')
-C=Ball(350,h/2,             Vector(1,1),1,40,'white')
-D=Ball(w/2,h/4,             Vector(1,1),5,20,'orange')
+B=Ball(500,200,             Vector(3,0),1,30,'red')
+C=Ball(350,h/2,             Vector(1,-3),1,40,'white')
+D=Ball(w/2,h/4,             Vector(4,1),5,20,'orange')
 E=Ball(w/2-70,h/2+50,       Vector(1,1),3,20,'black')
 F=Ball(w/2,h/2,             Vector(1,1),2,6,'yellow')
 I=Ball(w/2,h/2-200,         Vector(1,1),1,6,'red')
 Spoke1=Spoke(0,300,1000,300,[A])
 Rope1=Rope(200,A,B)
-Spoke=[Spoke1]
+Spoke=[]
 Spring=[]
-Rope=[Rope1]
-Game = [A,B]
+Rope=[]
+Game = [A,B,C,D]
 loop()
 c.mainloop()
