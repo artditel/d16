5589912143d0e95b495a1398ab25a690289f6914
Alex <savsmail@gmail.com>
2015-04-13 08:53:52 +0300

    class tests added

---
+++ common/fastTasks/classes.py
@@ -0,0 +1,26 @@
+###############################################################################
+#classes. Add your classes here.
+
+class HelloWorld:
+    def __init__(self, name="Anonymous"):
+        self.name = name
+    def get_hello(self):
+        return "Hello world from " + self.name
+
+
+###############################################################################
+#tests. Add your tests here.
+
+def test_hello_world():
+    hw = HelloWorld()
+    assert(hw.get_hello() == "Hello world from Anonymous")
+
+    hw = HelloWorld("Vasya")
+    assert(hw.get_hello() == "Hello world from Vasya")
+
+
+###############################################################################
+#main. Add test calls here.
+
+if __name__ == "__main__":
+    test_hello_world()
