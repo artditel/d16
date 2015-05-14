9b9405c79944db303a0e8d7a75d83828e002bd22
lbezel <bezellisa@yandex.ru>
2015-05-14 10:08:40 +0300

    Update robot.py

--- robot.py
+++ robot.py
@@ -7,5 +7,10 @@ size = 10
 for x in range(0, 150, 1):
 	for y in range(0, 100, 1):
 		c.create_rectangle( length + x * size , length + y * size, length + x * size - size,length +  y * size - size, fill = "saddle brown")
+def paint(i, j):
+		c.create_rectangle(i + size/2, j + size/2, i - size/2, j - size/2, fill = 'white')
+for i in c:
+	if c[i] = 'paint':
+		paint(ro_x, ro_y)
 
 c.mainloop()
