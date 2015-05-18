9c63a99963cf6534793b12953cb21586fdd819a1
agekht <scouser96@icloud.com>
2015-05-18 09:47:38 +0300

    Update Rationals.py

--- agekht/Rationals.py
+++ agekht/Rationals.py
@@ -1,22 +1,29 @@
+
 class Rationals():
     def __init__(self, m, n):
         self.m = m
         self.n = n
 
-    def add(self, arg):
+    def __add__(self, arg):
         x = self.m*arg.n + self.n*arg.m
         y = self.n*arg.n
-        return(x, y)
+        z = Rationals(x, y)
+        return(z)
 
-    def multiple(self, arg):
+    def __mul__(self, arg):
         x = self.m*arg.m
         y = self.n*arg.n
-        return(x, y)
+        z = Rationals(x, y)
+        return(z)
 
-    def divide(self, arg):
+    def __div__(self, arg):
         x = self.m*arg.n
         y = self.n*arg.m
-        return(x, y)
+        z = Rationals(x, y)
+        return(z)
+
+#    def __str__(self):
+
 
 #-------------
 
@@ -27,3 +34,7 @@ while x < 1000:
     x += 1
 
 print (a)
+z1 = Rationals(1,2)
+z2 = Rationals(3,4)
+z3 = Rationals(5,6)
+print(z1+z2+z3)
