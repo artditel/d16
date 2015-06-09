4fec51fa1444e5f69702aeffe74427e1bd1870f6
IHumonen <G1410ish@gmail.com>
2015-06-10 00:06:24 +0300

    only GUI

--- IHumonen/balda_game.py
+++ IHumonen/balda_game.py
@@ -11,10 +11,12 @@ def redraw(canvas):
 def starter(event):
 	first_word = ent.get()
 	for j in range(5):
-		field.create_text(j*size+size//2, 2*size+size//2, text=first_word[j])
 		letters[10 + j] = first_word[j]
+	redraw(field)
 	but.destroy()
 	lab.destroy()
+	lab_1 = Label(fra1, text=" Введите букву в строку, затем двойным кликом левой мышью выберите клетку для неё. \n  Выделите своё слово(клик левой клавишей мыши - начало и конец выделения) ", font="Arial 12")
+	lab_1.pack()
 	ent.delete(0, END)
 
 def mark(event):
@@ -28,7 +30,7 @@ def mark(event):
 		making_mark(x, y, 'red')
 
 
-def start_marking(event):
+def start_or_end_marking(event):
 	x = event.x
 	y = event.y
 	x = x // size * size
@@ -36,14 +38,10 @@ def start_marking(event):
 	number = x // size + y // size * cells
 	if mode[0] == 'mark':		
 		mode[0] = 'none'
+#		if current_word in dictionary:
 		redraw(field)
-		ent.delete(0, END)
 	else:
-		redraw(field)
-		if letters[number] == ' ':
-			letters[number] = ent.get()[0]
-			redraw(field)
-			making_mark(x, y, 'green')
+		if letters[number] != ' ':
 			mode[0] = 'mark'
 
 def making_mark(x, y, colour):
@@ -54,11 +52,48 @@ def making_mark(x, y, colour):
 	field.create_line(x_1, y, x_1, y_1, fill = colour)
 	field.create_line(x, y_1, x_1, y_1, fill = colour)
 def put_letter(event):
-	current_letter = ent.get()
-	
-	redraw(field)
+	x = event.x
+	y = event.y
+	x = x // size * size
+	y = y // size * size
+	number = x // size + y // size * cells
+	current_letter[0] = ent.get()
+	surroundings = True
+	if 0 < number < 4:
+		if letters[number + 1] == ' ' and letters[number - 1] == ' ' and letters[number + cells] == ' ':
+			surroundings = False
+	if 20 < number < 24:
+		if letters[number + 1] == ' ' and letters[number - 1] == ' ' and letters[number - cells] == ' ':
+			surroundings = False
+	if 0 == number:
+		if letters[number + 1] == ' ' and letters[number + cells] == ' ':
+			surroundings = False
+	if 4 == number:
+		if letters[number - 1] == ' ' and letters[number + cells] == ' ':
+			surroundings = False
+	if 20 == number:
+		if letters[number + 1] == ' ' and letters[number - cells] == ' ':
+			surroundings = False
+	if 24 == number:
+		if letters[number - 1] == ' ' and letters[number - cells] == ' ':
+			surroundings = False	
+	if current_letter[0].isalpha() and len(current_letter[0]) == 1 and surroundings:
+		letters[number] = current_letter[0]
+		redraw(field)
+		making_mark(x, y, 'green')
+		ent.delete(0, END)
+
+
 
 root = Tk()
+root_2 = Tk()
+
+tex = Text(root_2,width=20,
+		font="Verdana 12",
+		wrap=WORD)
+#scr = Scrollbar(root_2,command=tex.yview)
+#scr.pack() 
+tex.pack()
 
 ent = Entry(root, width=20, bd=5, fg = 'blue')
 
@@ -67,6 +102,7 @@ size = 50
 cells = side // size
 letters = [ ' ' for x in range (25)]
 mode = ['none']
+current_letter = [' ']
 
 
 field = tkinter.Canvas(width=side, height=side, bg = 'white')
@@ -74,12 +110,12 @@ field.pack()
 
 redraw(field)
 
-ent = Entry(root, width=20, bd=5, fg = 'blue')
 
 
 
+field.bind('<Double-Button-1>', put_letter )
 field.bind('<Motion>', mark)
-field.bind('<Button-1>', start_marking)
+field.bind('<Button-1>', start_or_end_marking)
 
 
 
@@ -109,7 +145,7 @@ field.bind('<Button-1>', start_marking)
 
 
 
-fra1 = Frame(root, width=500, height=100, bg="white", bd = 20)
+fra1 = Frame(root, width=500, height=200, bg="white", bd = 20)
 fra2 = Frame(root, width=300, height=200, bg="white", bd = 20)
 
 
@@ -135,5 +171,7 @@ fra2.pack()
 ent.pack()
 but.pack()
 lab.pack()
+
 field.mainloop()
 root.mainloop()
+root_2.mainloop()
